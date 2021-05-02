0aee0962d9b9f17c6afaf780e9a4df3d7cf3be8d
박선우 <sun960127@naver.com>
2021-05-02 23:51:44 +0900

    .

--- app/src/main/java/com/eomcs/web/ex04/Servlet02.java
+++ app/src/main/java/com/eomcs/web/ex04/Servlet02.java
@@ -22,7 +22,7 @@ public class Servlet02 extends GenericServlet {
     // - 웹 페이지의 폼(method='POST' 일 때)에서 전송 버튼을 클릭하면 POST 요청을 보낸다.
     //
     // 테스트
-    // - http://localhost:8080/java-web/ex04/test02.html 실행
+    // - http://localhost:8080/web/ex04/test02.html 실행
     //
 
     // 웹 브라우저가 보낸 데이터 읽기
@@ -68,7 +68,7 @@ public class Servlet02 extends GenericServlet {
 
     char[] chars = name.toCharArray();
     for (char c : chars) {
-      out.printf("%04x\n", (int) c);
+      out.printf("%x\n", (int) c);
     }
   }
 }
